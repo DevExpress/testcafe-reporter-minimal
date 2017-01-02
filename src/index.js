390277f0457cbb2ddc1a897b02a24463be7d4fbe
inikulin <ifaaan@gmail.com>
2017-01-03 00:03:26 +0300

    Add skipped tests support. Bump version.

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ export default function () {
         errDescriptors:     [],
         currentFixtureName: null,
         testCount:          0,
+        skipped:            0,
 
         reportTaskStart (startTime, userAgents, testCount) {
             this.testCount = testCount;
@@ -28,7 +29,17 @@ export default function () {
 
         reportTestDone (name, testRunInfo) {
             var hasErr = !!testRunInfo.errs.length;
-            var dot    = hasErr ? this.chalk.red('.') : '.';
+            var symbol = null;
+
+            if (testRunInfo.skipped) {
+                this.skipped++;
+
+                symbol = this.chalk.cyan('-');
+            }
+            else if (hasErr)
+                symbol = this.chalk.red('!');
+            else
+                symbol = '.';
 
             if (this.spaceLeft - 1 < 0) {
                 this.spaceLeft = this.viewportWidth - NEW_LINE.length - 1;
@@ -37,7 +48,7 @@ export default function () {
             else
                 this.spaceLeft--;
 
-            this.write(dot);
+            this.write(symbol);
 
             if (hasErr) {
                 this.errDescriptors = this.errDescriptors.concat(testRunInfo.errs.map(err => {
@@ -100,6 +111,11 @@ export default function () {
                 .newline()
                 .write(footer)
                 .newline();
+
+            if (this.skipped > 0) {
+                this.write(this.chalk.cyan(`${this.skipped} skipped`))
+                    .newline();
+            }
 
             if (warnings.length)
                 this._renderWarnings(warnings);
